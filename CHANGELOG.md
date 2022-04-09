9e158f37c2eea5fed750960e37561dade2928798
Om Ambarkar <heyom7@gmail.com>
2022-04-10 00:10:58 +0530

    updated with new website scraping much quicker

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.4
+## 0.1.0
 
 Get user public IP address with his approx location
--- lib/public_ip.dart
+++ lib/public_ip.dart
@@ -4,46 +4,25 @@ import 'package:http/http.dart' as http;
 import 'package:html/parser.dart';
 
 Future<void> _assignAllValues() async {
-  String theip;
   try {
-    const String _url = 'https://whatismyipaddress.com';
+    const String _url = 'https://www.ipaddress.my/';
     final _response = await http.get(Uri.parse(_url));
     final _html = parse(_response.body);
-
-    theip = _html.getElementById("ipv4")!.innerHtml;
-    theip = theip.split(">")[1].split("<")[0];
-    _publicIp = theip;
-    final String url = 'https://whatismyipaddress.com/ip/$_publicIp';
-    final response = await http.get(Uri.parse(url));
-    final html = parse(response.body);
-    var _allvals = html.getElementsByClassName("information");
-
-    _ASN =
-        _allvals[2].innerHtml.toString().split("<span>")[2].split("</span>")[0];
-    _ISP =
-        _allvals[3].innerHtml.toString().split("<span>")[2].split("</span>")[0];
-    _Services =
-        _allvals[4].innerHtml.toString().split("<span>")[2].split("</span>")[0];
-    _Country =
-        _allvals[6].innerHtml.toString().split("<span>")[2].split("</span>")[0];
-    _State =
-        _allvals[7].innerHtml.toString().split("<span>")[2].split("</span>")[0];
-    _City =
-        _allvals[8].innerHtml.toString().split("<span>")[2].split("</span>")[0];
-    _Longitude = _allvals[9]
-        .innerHtml
-        .toString()
-        .split("<span>")[2]
-        .split("</span>")[0]
-        .split("&")[0];
-    _Latitude = _allvals[10]
-        .innerHtml
-        .toString()
-        .split("<span>")[2]
-        .split("</span>")[0]
-        .split("&")[0];
+    var _new = _html.getElementsByTagName("td");
+    _publicIp = (_new[54].innerHtml);
+    _Country = (_new[56].innerHtml);
+    _State = (_new[58].innerHtml);
+    _City = (_new[60].innerHtml);
+    _ISP = (_new[62].innerHtml);
+    _ASN = (_new[62].innerHtml);
+    _Longitude = (_new[16].innerHtml);
+    _Latitude = (_new[14].innerHtml);
+    _TimeZone = (_new[18].innerHtml);
+    _LocalTime = (_new[20].innerHtml);
+    _Continent = (_new[80].innerHtml);
+    _Currency = (_new[86].innerHtml);
   } catch (e) {
-    theip = "error occurred while fetching";
+    print("Error Occured while fetching");
   }
 }
 
@@ -51,13 +30,36 @@ String? _publicIp;
 String? _ASN;
 String? _ISP;
 String? _Services;
+String? _Continent;
 String? _Country;
 String? _State;
 String? _City;
 String? _Latitude;
 String? _Longitude;
+String? _TimeZone;
+String? _LocalTime;
+String? _Currency;
 
-List _allValues = [];
+Future getTimeZone() async {
+  if (_TimeZone == null) {
+    await _assignAllValues();
+  }
+  return _TimeZone ?? "Error Fetching Time Zone";
+}
+
+Future getLocalTime() async {
+  if (_LocalTime == null) {
+    await _assignAllValues();
+  }
+  return _LocalTime ?? "Error Fetching Local Time";
+}
+
+Future getLocalCurrency() async {
+  if (_Currency == null) {
+    await _assignAllValues();
+  }
+  return _Currency ?? "Error Fetching Currency";
+}
 
 Future getPublicIp() async {
   if (_publicIp == null) {
